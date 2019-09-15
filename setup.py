fae19f2f12069600eae888be9bfb73334179b229
anmolwarman <43425855+anmolwarman@users.noreply.github.com>
2019-09-15 05:45:10 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,15 @@ from setuptools import setup
 
 setup(
     name = 'etepcl',
-    package = ['etepcl'],
-#     entry_points = {
-#             "console_scripts": ['etepcl = etepcl.cli']
-#         },
-    version = '1.00',
+    packages = ['etepcl'],
+    entry_points = {
+            "console_scripts": ['etepcl = etepcl.cli:main']
+        },
+    version = '1.3',
     description = 'Command line end to end processing',
     author = 'Anmol Warman',
     url = 'https://github.com/anmolwarman/etepcl',
+    download_url = 'https://github.com/hugorut/etepcl/tarball/1.3', 
     keywords = ['machine learning', 'ai', 'neural network'],
     setup_requires=["numpy"],
     install_requires=[
